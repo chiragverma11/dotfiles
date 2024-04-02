fe5430b0ad737947007596451ef7e6c6c50e5595
Chirag Verma <chiragverma1013@gmail.com>
2024-04-02 17:12:00 +0530

    ♻️ feat(nvim): make format_on_save a function to check for disable_autoformat first

--- .config/nvim/lua/custom/configs/conform.lua
+++ .config/nvim/lua/custom/configs/conform.lua
@@ -29,11 +29,13 @@ local options = {
   -- adding same formatter for multiple filetypes can look too much work for some
   -- instead of the above code you could just use a loop! the config is just a table after all!
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 3000, lsp_fallback = true }
+  end,
 }
 
 require("conform").setup(options)
