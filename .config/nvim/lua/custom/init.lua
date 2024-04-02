67489d971fe9387cd6d858c91f9a4aacde1554cc
Chirag Verma <chiragverma1013@gmail.com>
2024-04-02 17:13:34 +0530

    ✨ feat(nvim): add user_command for enable/disable autoformat_on_save

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -8,3 +8,22 @@
 vim.opt.relativenumber = true
 
 vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
+
+-- Commands to enable/disable autoformat-on-save
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
