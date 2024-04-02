86985eb3b452c195e56b2b7839f45a5b1a39393a
Chirag Verma <chiragverma1013@gmail.com>
2024-04-02 14:26:38 +0530

    ♻️ refactor(mappings): make conform formatter mapping to run async in nvim

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -8,9 +8,9 @@ M.general = {
     --  format with conform
     ["<leader>fm"] = {
       function()
-        require("conform").format()
+        require("conform").format { async = true, lsp_fallback = true }
       end,
-      "formatting",
+      "Format with conform",
     },
 
     -- LazyGit loader
