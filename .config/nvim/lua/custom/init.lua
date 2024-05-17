7b1725c7c88413404730ef587bc637c0f7b5c872
Chirag Verma <chiragverma1013@gmail.com>
2024-05-17 09:12:54 +0530

    ♻️ refactor(nvim): improve code structure in init.lua

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,16 +1,17 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
+local g = vim.g
 
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
-vim.opt.relativenumber = true
+-------------------------------------- globals -----------------------------------------
+g.vscode_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
 
-vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
+-------------------------------------- options ------------------------------------------
+opt.relativenumber = true
+
+-------------------------------------- commands ------------------------------------------
+local new_cmd = vim.api.nvim_create_user_command
 
 -- Commands to enable/disable autoformat-on-save
-vim.api.nvim_create_user_command("FormatDisable", function(args)
+new_cmd("FormatDisable", function(args)
   if args.bang then
     -- FormatDisable! will disable formatting just for this buffer
     vim.b.disable_autoformat = true
@@ -21,9 +22,18 @@ end, {
   desc = "Disable autoformat-on-save",
   bang = true,
 })
-vim.api.nvim_create_user_command("FormatEnable", function()
+
+new_cmd("FormatEnable", function()
   vim.b.disable_autoformat = false
   vim.g.disable_autoformat = false
 end, {
   desc = "Re-enable autoformat-on-save",
 })
+
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
