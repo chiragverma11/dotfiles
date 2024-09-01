51c335e7820873e553b4030f61663159ee5d73ca
Chirag Verma <chiragverma1013@gmail.com>
2024-09-01 08:43:09 +0530

    🩹 fix(nvim): disable better-escape keymap for jk in terminal mode

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -26,7 +26,15 @@ return {
     "max397574/better-escape.nvim",
     event = "InsertEnter",
     config = function()
-      require("better_escape").setup()
+      require("better_escape").setup {
+        mappings = {
+          t = {
+            j = {
+              k = false,
+            },
+          },
+        },
+      }
     end,
   },
 
