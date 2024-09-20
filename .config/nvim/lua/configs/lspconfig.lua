783d9943d5dc5a522e637de1a222751e4c79c5d9
Chirag Verma <chiragverma1013@gmail.com>
2024-09-20 08:45:43 +0530

    ♻️ refactor(nvim): make `servers` a table for easy custom configuration

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -3,56 +3,63 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
--- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "ts_ls", "tailwindcss", "eslint", "prismals", "marksman" }
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
-
-lspconfig.jsonls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "json", "jsonc" },
-  settings = {
-    json = {
-      schemas = {
-        {
-          fileMatch = { "package.json" },
-          url = "https://json.schemastore.org/package.json",
-        },
-        {
-          fileMatch = { "tsconfig*.json" },
-          url = "https://json.schemastore.org/tsconfig.json",
-        },
-        {
-          fileMatch = {
-            ".prettierrc",
-            ".prettierrc.json",
-            "prettier.config.json",
+local servers = {
+  html = {},
+  cssls = {},
+  ts_ls = {},
+  tailwindcss = {},
+  eslint = {},
+  prismals = {},
+  marksman = {},
+  jsonls = {
+    filetypes = { "json", "jsonc" },
+    settings = {
+      json = {
+        schemas = {
+          {
+            fileMatch = { "package.json" },
+            url = "https://json.schemastore.org/package.json",
           },
-          url = "https://json.schemastore.org/prettierrc.json",
-        },
-        {
-          fileMatch = { ".eslintrc", ".eslintrc.json" },
-          url = "https://json.schemastore.org/eslintrc.json",
-        },
-        {
-          fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
-          url = "https://json.schemastore.org/babelrc.json",
-        },
-        {
-          fileMatch = {
-            ".stylelintrc",
-            ".stylelintrc.json",
-            "stylelint.config.json",
+          {
+            fileMatch = { "tsconfig*.json" },
+            url = "https://json.schemastore.org/tsconfig.json",
           },
-          url = "http://json.schemastore.org/stylelintrc.json",
+          {
+            fileMatch = {
+              ".prettierrc",
+              ".prettierrc.json",
+              "prettier.config.json",
+            },
+            url = "https://json.schemastore.org/prettierrc.json",
+          },
+          {
+            fileMatch = { ".eslintrc", ".eslintrc.json" },
+            url = "https://json.schemastore.org/eslintrc.json",
+          },
+          {
+            fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+            url = "https://json.schemastore.org/babelrc.json",
+          },
+          {
+            fileMatch = {
+              ".stylelintrc",
+              ".stylelintrc.json",
+              "stylelint.config.json",
+            },
+            url = "http://json.schemastore.org/stylelintrc.json",
+          },
         },
       },
     },
   },
 }
+
+for server, config in pairs(servers) do
+  local server_disabled = (config.disabled ~= nil and config.disabled) or false
+
+  if not server_disabled then
+    lspconfig[server].setup(
+      vim.tbl_deep_extend("force", { on_attach = on_attach, capabilities = capabilities }, config)
+    )
+  end
+end
