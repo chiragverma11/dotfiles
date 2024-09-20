caada892b000c172284ea868c27340ec2a2da9ea
Chirag Verma <chiragverma1013@gmail.com>
2024-09-20 10:14:02 +0530

    ♻️ refactor(nvim): configure markdown formatter

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -2,6 +2,26 @@
 local options = {
   lsp_format = "fallback",
 
+  formatters = {
+    ["markdown-toc"] = {
+      condition = function(_, ctx)
+        for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+          if line:find "<!%-%- toc %-%->" then
+            return true
+          end
+        end
+      end,
+    },
+    ["markdownlint-cli2"] = {
+      condition = function(_, ctx)
+        local diag = vim.tbl_filter(function(d)
+          return d.source == "markdownlint"
+        end, vim.diagnostic.get(ctx.buf))
+        return #diag > 0
+      end,
+    },
+  },
+
   formatters_by_ft = {
     lua = { "stylua" },
 
@@ -19,8 +39,8 @@ local options = {
     json = { "prettierd", "prettier", stop_after_first = true },
     jsonc = { "prettierd", "prettier", stop_after_first = true },
     yaml = { "prettierd", "prettier", stop_after_first = true },
-    markdown = { "prettierd", "prettier", stop_after_first = true },
-    ["markdown.mdx"] = { "prettierd", "prettier", stop_after_first = true },
+    ["markdown"] = { "prettierd", "markdownlint-cli2", "markdown-toc" },
+    ["markdown.mdx"] = { "prettierd", "markdownlint-cli2", "markdown-toc" },
     graphql = { "prettierd", "prettier", stop_after_first = true },
 
     sh = { "shfmt" },
