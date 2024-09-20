52bee4efb13dacdc57abb5f1884156e313eedff8
Chirag Verma <chiragverma1013@gmail.com>
2024-09-20 08:13:52 +0530

    ✨ feat(nvim): add `jsonls` lspconfig for json files

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -12,3 +12,47 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.jsonls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "json", "jsonc" },
+  settings = {
+    json = {
+      schemas = {
+        {
+          fileMatch = { "package.json" },
+          url = "https://json.schemastore.org/package.json",
+        },
+        {
+          fileMatch = { "tsconfig*.json" },
+          url = "https://json.schemastore.org/tsconfig.json",
+        },
+        {
+          fileMatch = {
+            ".prettierrc",
+            ".prettierrc.json",
+            "prettier.config.json",
+          },
+          url = "https://json.schemastore.org/prettierrc.json",
+        },
+        {
+          fileMatch = { ".eslintrc", ".eslintrc.json" },
+          url = "https://json.schemastore.org/eslintrc.json",
+        },
+        {
+          fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+          url = "https://json.schemastore.org/babelrc.json",
+        },
+        {
+          fileMatch = {
+            ".stylelintrc",
+            ".stylelintrc.json",
+            "stylelint.config.json",
+          },
+          url = "http://json.schemastore.org/stylelintrc.json",
+        },
+      },
+    },
+  },
+}
