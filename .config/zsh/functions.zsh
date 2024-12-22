88825eec40090f162198197463746fde307613aa
Chirag Verma <chiragverma1013@gmail.com>
2024-12-22 15:45:37 +0530

    ✨ feat(zsh): add yazi shell wrapper & sesh-sessions function

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,1 +1,21 @@
-		cd -- "$cwd"
+# Yazi shell wrapper that provides the ability to change the current working directory when exiting Yazi.
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
+# sesh
+function sesh-sessions() {
+  {
+    exec </dev/tty
+    exec <&1
+    local session
+    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+    [[ -z "$session" ]] && return
+    sesh connect $session
+  }
+}
