5639d820b60143309b2934d046535d4908d5d2e2
Chirag Verma <chiragverma1013@gmail.com>
2024-12-22 15:53:22 +0530

    ✨ feat(zsh): add keybindings to trigger `sesh-sessions` function using `Alt+s`

--- .config/zsh/keybinds.zsh
+++ .config/zsh/keybinds.zsh
@@ -1,3 +1,9 @@
 # zsh-autosuggestions
 # Accept suggestion with Alt-c
 bindkey '^[c' autosuggest-accept
+
+# sesh
+zle     -N             sesh-sessions
+bindkey -M emacs '\es' sesh-sessions
+bindkey -M vicmd '\es' sesh-sessions
+bindkey -M viins '\es' sesh-sessions
