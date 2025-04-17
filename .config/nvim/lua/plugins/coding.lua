7ded212e7ebf9c63ca845c89209fb21ef9bed51c
Chirag Verma <chiragverma1013@gmail.com>
2025-04-17 18:49:29 +0530

    refactor(nvim): move `nvim-ts-context-commentstring` plugin as a dep. to `Comment.nvim` plugin

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -3,22 +3,22 @@ return {
   {
     "numToStr/Comment.nvim",
     event = "BufReadPost",
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+    dependencies = {
+      {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        dependencies = { "numToStr/Comment.nvim" },
+        opts = {
+          enable_autocmd = false,
+        },
+        config = function(_, opts)
+          require("ts_context_commentstring").setup(opts)
+        end,
+      },
+    },
     config = function()
       require("Comment").setup {
         pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
       }
-    end,
-  },
-
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    dependencies = { "numToStr/Comment.nvim" },
-    opts = {
-      enable_autocmd = false,
-    },
-    config = function(_, opts)
-      require("ts_context_commentstring").setup(opts)
     end,
   },
 
