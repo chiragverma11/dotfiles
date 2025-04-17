685bc4f8e6f4225baa72d6813aa6c763dd1e7dc8
Chirag Verma <chiragverma1013@gmail.com>
2025-04-17 20:37:02 +0530

    feat(nvim): add `harpoon` plugin

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,57 @@
+---@type NvPluginSpec[]
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
+  keys = function()
+    local keys = {
+      {
+        "<leader>H",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Harpoon File",
+      },
+      {
+        "<leader>hh",
+        function()
+          local harpoon = require "harpoon"
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon Quick Menu",
+      },
+      {
+        "<leader>hp",
+        function()
+          require("harpoon"):list():prev()
+        end,
+        desc = "Harpoon Previous File",
+      },
+      {
+        "<leader>hn",
+        function()
+          require("harpoon"):list():next()
+        end,
+        desc = "Harpoon Next File",
+      },
+    }
+
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Harpoon to File " .. i,
+      })
+    end
+    return keys
+  end,
+}
