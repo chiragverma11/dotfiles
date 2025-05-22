8433f97c8594b62e3ff8786eb78524cca02b795d
Chirag Verma <chiragverma1013@gmail.com>
2025-05-22 19:37:06 +0530

    refactor(nvim): move `markdown-preview.nvim` plugin to markdown.lua file

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -4,13 +4,14 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = function()
+      require("lazy").load { plugins = { "markdown-preview.nvim" } }
       vim.fn["mkdp#util#install"]()
     end,
     keys = {
       {
-        "<leader>mp",
+        "<leader>cp",
         ft = "markdown",
-        ":MarkdownPreviewToggle<CR>",
+        "<cmd>MarkdownPreviewToggle<cr>",
         desc = "Markdown Preview",
       },
     },
