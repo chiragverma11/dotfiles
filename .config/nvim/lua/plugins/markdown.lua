80bb99a9a559e1365c26a9b36ceb9d7bb21ac95f
Chirag Verma <chiragverma1013@gmail.com>
2025-05-22 19:39:12 +0530

    feat(nvim): add `render-markdown.nvim` plugin

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -19,4 +19,25 @@ return {
       vim.cmd [[do FileType]]
     end,
   },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = false,
+      },
+    },
+  },
 }
