89423e55a1a4b68f50b50e495e7c44f450058d1d
Chirag Verma <chiragverma1013@gmail.com>
2026-03-15 09:24:47 +0530

    feat(nvim): add `hardtime.nvim` plugin

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,11 @@
+---@type NvPluginSpec[]
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {
+      disable_mouse = false,
+    },
+  },
+}
